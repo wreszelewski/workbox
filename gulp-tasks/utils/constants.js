b81ea66873c44d379987902ef75271be65ad50fb
Matt Gaunt <matt@gauntface.co.uk>
2017-08-16 10:41:34 -0700

    Adding v3-errors

--- gulp-tasks/utils/constants.js
+++ gulp-tasks/utils/constants.js
@@ -2,8 +2,8 @@ module.exports = {
   // This is a directory that should not be commited
   // to git and will be removed and rebuilt between
   // test runs.
-  PACKAGE_BUILD_DIRNAME: 'build',
-  BROWSER_BUILD_DIRNAME: 'browser-bundles',
+  PACKAGE_BUILD_DIRNAME: 'dist',
+  BROWSER_BUILD_DIRNAME: 'browser',
   TEST_BUNDLES_BUILD_DIRNAME: 'bundle-builds',
 
   // This is the environments that we should use for NODE_ENV.
